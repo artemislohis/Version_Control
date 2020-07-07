00e792e313705500d30f7b04eb5094dd1db4dd64
amitis <amitisshokrollahi@yahoo.com>
2020-07-07 12:36:51 +0430

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Version_Control.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Version_Control.dir/main.c.obj"
+  "Version_Control.exe"
+  "Version_Control.exe.manifest"
+  "Version_Control.lib"
+  "Version_Control.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Version_Control.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
